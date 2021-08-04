766dcc551678a5678a0071b0a7f08803527fcb72
ZeroCore <esetting@hotmail.com>
2021-08-04 12:04:09 +0800

    stop edit

--- html/foot.php
+++ html/foot.php
@@ -3,7 +3,7 @@
 	<ul>
 		<li><a href="https://www.wjx.cn/vj/PWLcF5K.aspx" target="_blank">反馈意见</a></li>
 		<li><a href="#" target="_blank">支持我吧</a></li>
-		<li>继续开发。处理时间：2021/7/31</li>
+		<li>停止开发。处理时间：2021/8/4</li>
 	</ul>
 </div>
  
