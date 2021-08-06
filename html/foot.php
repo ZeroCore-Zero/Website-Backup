cd9ff2a19f1b209c536131acd52eda26b059742a
ZeroCore <esetting@hotmail.com>
2021-08-06 22:43:18 +0800

    i forget it

--- html/foot.php
+++ html/foot.php
@@ -3,7 +3,7 @@
 	<ul>
 		<li><a href="https://www.wjx.cn/vj/PWLcF5K.aspx" target="_blank">反馈意见</a></li>
 		<li><a href="//8.131.70.189/html/pay.php" target="_blank">支持我吧</a></li>
-		<li><script type="text/javascript" src="https://s4.cnzz.com/z_stat.php?id=1280189479&web_id=1280189479"></script></li>
+		<li><script type="text/javascript" src="//s4.cnzz.com/z_stat.php?id=1280189479&web_id=1280189479"></script></li>
 		<li>停止开发。处理时间：2021/8/4</li>
 	</ul>
 </div>
