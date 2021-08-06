de11087b6254d86c10d9f4be99d7042543554ffd
Server <NONEr>
2021-08-06 20:04:35 +0800

    优化页面布局

--- html/head.php
+++ html/head.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8" />
+		<meta charset="utf-8">
 		<link rel="stylesheet" href="//8.131.70.189/css/web.css">
+		<link rel="stylesheet" href="//8.131.70.189/css/head.css">
+		<link rel="stylesheet" href="//8.131.70.189/css/main.css">
+		<link rel="stylesheet" href="//8.131.70.189/css/foot.css">
 		<link rel="stylesheet" href="//8.131.70.189/css/login.css">
 		<link rel="icon" href="//8.131.70.189/html/favicon.ico">
 		<title><?php
@@ -18,12 +21,19 @@
 	</head>
 	<body>
 		<div class="head">
-			<ul>
-				<li><a href="//8.131.70.189"><img src="//8.131.70.189/html/Logo.png" height="36rem" style="vertical-align:middle"></img></a></li>
-				<li><a href="//8.131.70.189">首页</a></li>
-				<li><a href="//8.131.70.189/account">账号系统</a></li>
-				<li><a href="//8.131.70.189/login">登录</a></li>
-				<li><iframe id="music" frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=5146554&auto=1&height=66"></iframe></li>
-			</ul>
+			<div>
+				<ul>
+					<li><a href="//8.131.70.189"><img src="//8.131.70.189/html/Logo-alpha.png" height="36rem" style="vertical-align:middle"></img></a></li>
+					<li><a href="//8.131.70.189">首页</a></li>
+					<li><a href="//8.131.70.189/account">账号系统</a></li>
+				</ul>
+			</div>
+			<div>
+				<ul>
+					<li><a href="//8.131.70.189/login">登录</a></li>
+					<li><iframe frameborder="no" width="86" height="86" style="vertical-align:middle" src="//music.163.com/outchain/player?type=2&id=5146554&auto=1&height=66"></iframe></li>
+				</ul>
+			</div>
 		</div>
+
 		<div class="main">
