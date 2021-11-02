0a05c1a63eb0f0a6032eaeb302ed949b47897305
ZeroCore <esetting@hotmail.com>
2021-11-02 23:41:26 +0800

    change domain

--- html/head.php
+++ html/head.php
@@ -2,12 +2,12 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<link rel="stylesheet" href="//8.131.70.189/css/web.css">
-		<link rel="stylesheet" href="//8.131.70.189/css/head.css">
-		<link rel="stylesheet" href="//8.131.70.189/css/main.css">
-		<link rel="stylesheet" href="//8.131.70.189/css/foot.css">
-		<link rel="stylesheet" href="//8.131.70.189/css/login.css">
-		<link rel="icon" href="//8.131.70.189/html/favicon.ico">
+		<link rel="stylesheet" href="//www.zerocore.xyz/css/web.css">
+		<link rel="stylesheet" href="//www.zerocore.xyz/css/head.css">
+		<link rel="stylesheet" href="//www.zerocore.xyz/css/main.css">
+		<link rel="stylesheet" href="//www.zerocore.xyz/css/foot.css">
+		<link rel="stylesheet" href="//www.zerocore.xyz/css/login.css">
+		<link rel="icon" href="//www.zerocore.xyz/html/favicon.ico">
 		<title><?php
 				if (defined("TITLE"))
 				{
@@ -23,14 +23,14 @@
 		<div class="head">
 			<div>
 				<ul>
-					<li><a href="//8.131.70.189"><img src="//8.131.70.189/html/Logo-alpha.png" height="36rem" style="vertical-align:middle"></img></a></li>
-					<li><a href="//8.131.70.189">首页</a></li>
-					<li><a href="//8.131.70.189/account">账号系统</a></li>
+					<li><a href="//www.zerocore.xyz"><img src="//www.zerocore.xyz/html/Logo-alpha.png" height="36rem" style="vertical-align:middle"></img></a></li>
+					<li><a href="//www.zerocore.xyz">首页</a></li>
+					<li><a href="//www.zerocore.xyz/account">账号系统</a></li>
 				</ul>
 			</div>
 			<div>
 				<ul>
-					<li><a href="//8.131.70.189/login">登录</a></li>
+					<li><a href="//www.zerocore.xyz/login">登录</a></li>
 					<li><iframe frameborder="no" width="86" height="86" style="vertical-align:middle" src="//music.163.com/outchain/player?type=2&id=5146554&auto=0&height=66"></iframe></li>
 				</ul>
 			</div>
--- html/pay.php
+++ html/pay.php
@@ -5,6 +5,6 @@ require "head.php";
 
 <h1>请有能力的大佬捐助</h1>
 <p style="color:#7c7c7c;text-align:center">毕竟服务器运行也是需要成本的QAQ(小声bb)</p>
-<img src="//8.131.70.189/html/pay-card.png" width="100%"></img>
+<img src="//www.zerocore.xyz/html/pay-card.png" width="100%"></img>
 
 <?php require "foot.php"; ?>
